3b9446db1db08cda962d1712ee300515e872b1ae
RoarRain <RoarRain@163.com>
2015-08-02 12:50:08 +0800

    Initial Commit

---
+++ messageTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  messageTest
+//
+//  Created by RoarRain on 15/8/2.
+//  Copyright (c) 2015年 RoarRain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
